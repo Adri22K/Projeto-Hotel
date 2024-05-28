4dfa48ff1ca350a74592744fff14b8e1016ebc25
Adri22K <168793109+Adri22K@users.noreply.github.com>
2024-05-28 19:20:57 -0300

    Update Fisico.sql

--- Banco de Dados/Fisico.sql
+++ Banco de Dados/Fisico.sql
@@ -2,7 +2,7 @@ CREATE TABLE Hospedes(
 	id_hospedes INT PRIMARY KEY NOT NULL,
 	nome_completo VARCHAR(100) NOT NULL,
 	dt_nascimento DATE NOT NULL,
-	endereço VARCHAR(100) NOT NULL,
+	endereco VARCHAR(100) NOT NULL,
 	telefone VARCHAR(15) NOT NULL,
 	email VARCHAR(50),
 	cpf VARCHAR(15) NOT NULL
@@ -13,17 +13,17 @@ CREATE TABLE Funcionarios(
 	id_funcionario INT PRIMARY KEY NOT NULL,
 	nome_completo VARCHAR(100) NOT NULL,
 	cargo VARCHAR(25) NOT NULL,
-	telefone VARCHAR(15) NOT NULL,
-	id_reservas INT NOT NULL
+	telefone VARCHAR(15) NOT NULL
+	
 	
 );
 
 CREATE TABLE Tipo_Quarto(
 	id_tipo INT PRIMARY KEY NOT NULL,
 	caracteristica VARCHAR(50) NOT NULL,
-	descrição VARCHAR(50) NOT NULL,
+	descricao VARCHAR(50) NOT NULL,
 	capacidade INT NOT NULL,
-	preço_noite NUMERIC(5,2) NOT NULL
+	preco_noite NUMERIC(5,2) NOT NULL
 	
 );
 
@@ -31,7 +31,7 @@ CREATE TABLE Quarto(
 	id_quarto INT PRIMARY KEY NOT NULL,
 	num_quarto INT NOT NULL,
 	tipo VARCHAR(25) NOT NULL,
-	preço NUMERIC(5,2) NOT NULL,
+	preco NUMERIC(5,2) NOT NULL,
 	id_tipo INT NOT NULL,
 
 CONSTRAINT id_tipoFK FOREIGN KEY (id_tipo)
@@ -43,7 +43,7 @@ REFERENCES Tipo_Quarto (id_tipo)
 
 -- CREATE TABLE DE RELACIONAMENTO
 CREATE TABLE Reservas(
-	id_reservas INT PRIMARY KEY NOT NULL,
+	id_reservas SERIAL PRIMARY KEY NOT NULL,
 	data_checkin DATE NOT NULL,
 	data_checkout DATE NOT NULL,
 	status VARCHAR(10) NOT NULL,
@@ -57,6 +57,4 @@ CONSTRAINT id_quartoFK FOREIGN KEY (id_quarto) REFERENCES Quarto (id_quarto)
 
 );
 
--- ADICIONAR RELAÇÃO RESERVAS A TABELA FUNCIONARIOS
-ALTER TABLE Funcionarios ADD CONSTRAINT id_reservasFK
-FOREIGN KEY(id_reservas) REFERENCES Reservas(id_reservas);
+
